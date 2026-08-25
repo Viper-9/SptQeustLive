4230b495c34062dac6644dfd9e8742ba38d2d05e
Viper-9 <Viper-9@users.noreply.github.com>
2026-08-25 23:07:13 +0900

    Add hideout production overrides and trader assort updates

--- server/HideoutFixes.cs
+++ server/HideoutFixes.cs
@@ -1,9 +1,13 @@
 using System.Reflection;
+using HarmonyLib;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers.Commerce;
 using SPTarkov.Server.Core.Helpers.Server;
 using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Eft.Hideout;
+using SPTarkov.Server.Core.Models.Enums.Hideout;
 using SPTarkov.Server.Core.Models.Spt.Tables;
 
 namespace SptQuestLive;
@@ -20,27 +24,51 @@ public class HideoutFixesLoader(
         var modPath = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
         var overrideFilePath = System.IO.Path.Combine(modPath, OverrideFileRelativePath);
 
-        if (!File.Exists(overrideFilePath))
+        if (File.Exists(overrideFilePath))
         {
-            return Task.CompletedTask;
-        }
+            var overrides = modHelper.GetJsonDataFromFile<Dictionary<MongoId, HideoutProduction>>(modPath, OverrideFileRelativePath);
+            var recipes = hideoutTable.Production.Recipes;
 
-        var overrides = modHelper.GetJsonDataFromFile<Dictionary<MongoId, HideoutProduction>>(modPath, OverrideFileRelativePath);
-        var recipes = hideoutTable.Production.Recipes;
-
-        foreach (var (recipeId, recipe) in overrides)
-        {
-            var index = recipes.FindIndex(r => r.Id == recipeId);
-            if (index >= 0)
+            foreach (var (recipeId, recipe) in overrides)
             {
-                recipes[index] = recipe;
-            }
-            else
-            {
-                recipes.Add(recipe);
+                var index = recipes.FindIndex(r => r.Id == recipeId);
+                if (index >= 0)
+                {
+                    recipes[index] = recipe;
+                }
+                else
+                {
+                    recipes.Add(recipe);
+                }
             }
         }
+
+        var harmony = new Harmony("com.viper.sptquestlive");
+        harmony.Patch(
+            AccessTools.Method(typeof(RewardHelper), nameof(RewardHelper.GetRewardProductionMatch)),
+            prefix: new HarmonyMethod(typeof(ProductionRewardMatchPatch), nameof(ProductionRewardMatchPatch.Prefix)));
 
         return Task.CompletedTask;
+    }
+}
+
+public static class ProductionRewardMatchPatch
+{
+    private static readonly MethodInfo GetMatchingProductions =
+        AccessTools.Method(typeof(RewardHelper), "GetMatchingProductions");
+
+    public static bool Prefix(RewardHelper __instance, Reward craftUnlockReward, MongoId questId, ref List<HideoutProduction> __result)
+    {
+        var traderId = craftUnlockReward.TraderId;
+        var text = traderId?.Int?.ToString() ?? traderId?.String;
+        if (text is null || !int.TryParse(text, out var areaTypeInt))
+        {
+            return true;
+        }
+
+        var desiredHideoutAreaType = (HideoutAreas)areaTypeInt;
+        __result = (List<HideoutProduction>)GetMatchingProductions.Invoke(
+            __instance, new object[] { desiredHideoutAreaType, questId, craftUnlockReward })!;
+        return false;
     }
 }
